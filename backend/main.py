326bf4e237edb137dc0fdb282a53814ba198da71
Sujan  Timalsina <iamsjn44@gmail.com>
2026-04-18 17:44:59 +0545

    Updated register api

--- backend/main.py
+++ backend/main.py
@@ -122,7 +122,6 @@ def get_password_hash(password: str):
 
 
 @app.post("/register")
-@app.post("/register/")
 async def register(data: Registration):
     try:
         user_data = data.model_dump()   # ✅ FIXED (NOT dict())
